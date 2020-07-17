e01fdae30522e48c97be5109c4f2963d6cff2af6
iorkenfew <zhenyatavr2626@gmal.com>
2020-07-17 08:38:32 +0400

    yrok

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,11 +3,25 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Lar</title>
+        <title>Larareact</title>
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     </head>
     <body>
-            <h1>TEST<h1>
+        <h1>Hello world</h1>
+        <button onclick="testAjax()">test ajax</button>
     </body>
-</html>
 
+    <script>
+        function testAjax()
+        {
+            axios.get('/test')
+            .then(function (response) {
+            console.log(response.data);
+            })
+            .catch(function (error)
+            {
+                alert('its broken')
+            })
+        }
+    </script>
+</html>
